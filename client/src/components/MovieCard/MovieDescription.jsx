18e77aa85d43865b82de1ef00541c0fcdb4d34c1
ZackJowett <zjwebdev@gmail.com>
2022-10-06 19:40:11 +1100

    show full description when more detail is open

--- client/src/components/MovieCard/MovieDescription.jsx
+++ client/src/components/MovieCard/MovieDescription.jsx
@@ -1,10 +1,10 @@
 import { Card } from "react-bootstrap";
 
-const MovieDescription = ({ content }) => {
+const MovieDescription = ({ content, showFull }) => {
 	const TEXT_LENGTH = 100;
 
 	// Reduce length of content
-	if (content.length > TEXT_LENGTH) {
+	if (!showFull && content.length > TEXT_LENGTH) {
 		content = content.slice(0, TEXT_LENGTH) + "...";
 	}
 
