a67d7e684082e771e6aa758175a63c92dc4a096d
ZackJowett <zjwebdev@gmail.com>
2022-10-06 19:39:35 +1100

    renamed values and set defaults

--- models/filmCollection.js
+++ models/filmCollection.js
@@ -2,17 +2,26 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const filmCollectionSchema = new Schema({
-	Title: String,
-	DateCreated: { type: Date, default: Date.now },
-	Films: [
-		ObjectId
-	],
-	Favourite: [
-		ObjectId
-	],
-	RecentlyAdded: [
-		ObjectId
-	]
+	title: String,
+	dateCreated: { type: Date, default: Date.now },
+	films: {
+		type: [
+			String
+		],
+		default: []
+	},
+	favourites: {
+		type: [
+			String
+		],
+		default: []
+	},
+	recentlyAdded: {
+		type: [
+			String
+		],
+		default: []
+	}
 });
 
 const FilmCollection = mongoose.model("FilmCollection", filmCollectionSchema);
