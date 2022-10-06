79265551324aea1412968de43be050e83472fa6f
ZackJowett <zjwebdev@gmail.com>
2022-10-06 19:40:32 +1100

    added save route and updated searchfilm name

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2,11 +2,20 @@
 const express = require("express");
 const apiRouter = express.Router();
 const apiController = require("../controllers/apiController.js");
+// const collectionController
 
 // Routes
-apiRouter.get("/searchFilm/:film", (req, res) =>
+apiRouter.get("/search/:film", (req, res) =>
 	apiController.searchFilm(req, res)
 );
+
+apiRouter.post("/save", (req, res) => apiController.saveFilm(req, res));
+// apiRouter.post("/collections/:", (req, res) =>
+// 	apiController.viewCollection(req, res)
+// );
+// apiRouter.post("/collections", (req, res) =>
+// 	apiController.viewAllCollections(req, res)
+// );
 
 // Export
 module.exports = { apiRouter };
