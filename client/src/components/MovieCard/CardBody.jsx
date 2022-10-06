a8c1c5cab27fb10c7a04b1152189f59fbdd27dc2
ZackJowett <zjwebdev@gmail.com>
2022-10-06 19:38:10 +1100

    added save movie button

--- client/src/components/MovieCard/CardBody.jsx
+++ client/src/components/MovieCard/CardBody.jsx
@@ -8,48 +8,77 @@ const CardBody = ({ currData }) => {
 	const [
 		showInfo,
 		setShowInfo
-	] = useState({ label: currData.id, value: false });
+	] = useState({ label: currData._id, value: false });
 
 	const updateShowInfo = () => {
 		console.log(showInfo);
 		setShowInfo({ ...showInfo, value: !showInfo.value });
 	};
 
+	const saveToCollection = () => {
+		const requestOptions = {
+			method: "POST"
+		};
+
+		fetch(`api/save?title=test&id=${currData._id}`, requestOptions);
+	};
+
 	return (
-		<Card.Body>
-			<div className="movie-card-title-wrapper">
-				<Card.Title className="movie-card-title">
-					{currData.title}
-				</Card.Title>
+		<div class="movie-card-body-wrapper">
+			<Card.Body>
+				<div className="movie-card-title-wrapper">
+					<Card.Title className="movie-card-title">
+						{currData.title}
+					</Card.Title>
 
-				<Card.Subtitle>
-					{currData.genres.map((genre, index) => (
-						<span key={genre.id}>
-							{/* Add / between genres */}
-							{index == 0 ? genre.name : " / " + genre.name}
-						</span>
+					<Card.Subtitle>
+						{currData.genres.map((genre, index) => (
+							<span key={genre.id}>
+								{/* Add / between genres */}
+								{index == 0 ? genre.name : " / " + genre.name}
+							</span>
+						))}
+					</Card.Subtitle>
+
+					{/* OMDb Ratings */}
+					{currData.ratings.map((rating) => (
+						<Badge bg="primary" className="movie-card-badge" pill>
+							{rating.Value}
+						</Badge>
 					))}
-				</Card.Subtitle>
 
-				{currData.vote_average ? (
+					{/* TMDb Ratins */}
 					<Badge bg="primary" className="movie-card-badge" pill>
-						{currData.vote_average.toFixed(1) + "/10"}
+						{currData.vote_average != 0 ? (
+							currData.vote_average.toFixed(1) + "/10"
+						) : null}
 					</Badge>
-				) : null}
-			</div>
+				</div>
 
-			<MovieDescription content={currData.overview} />
+				<MovieDescription
+					content={currData.overview}
+					showFull={showInfo.value}
+				/>
 
-			<Button
-				variant="outline-info"
-				onClick={updateShowInfo}
-				className="movie-card-button"
-			>
-				{!showInfo.value ? "Details" : "Hide"}
-			</Button>
-
+				<div class="card-buttons">
+					<Button
+						variant="outline-info"
+						onClick={updateShowInfo}
+						className="movie-card-button"
+					>
+						{!showInfo.value ? "Details" : "Hide"}
+					</Button>
+					<Button
+						variant="info"
+						onClick={saveToCollection}
+						className="movie-card-button movie-card-save"
+					>
+						Save
+					</Button>
+				</div>
+			</Card.Body>
 			{!showInfo.value ? "" : <AdditionalInfo movie={currData} />}
-		</Card.Body>
+		</div>
 	);
 };
 
